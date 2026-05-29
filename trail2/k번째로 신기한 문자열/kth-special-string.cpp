1aec519fafe3a08e1e443cf26ae09762ef15a9d7
HEECHANG SON <73211854+mikeylili@users.noreply.github.com>
2026-05-29 14:28:39 +0900

    [Passed] k번째로 신기한 문자열 (2ms, 0MB)

---
+++ trail2/k번째로 신기한 문자열/kth-special-string.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N, K;
+    string T;
+    cin >> N;
+    cin >> K;
+    cin >> T;
+    string AS [101];
+    int s = 0;
+    for (int i = 0; i < N; i++){
+        string S;
+        cin >> S;
+        if (S.starts_with(T)) {AS[s] = S; s++;}
+    }
+    sort(AS, AS+s);
+    cout << AS[K-1];
+    return 0;
+}
