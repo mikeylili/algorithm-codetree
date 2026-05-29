8b328250098b3c1a2a9598cd0ab607be1e9a2b45
HEECHANG SON <73211854+mikeylili@users.noreply.github.com>
2026-05-29 20:34:44 +0900

    [Passed] 겹치지 않는 사각형의 넓이 (125ms, 16MB)

---
+++ trail2/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int arr[2001][2001] = {0};
+void paint1(int x1, int y1, int x2, int y2){
+    for (int i = x1; i < x2; i++){
+        for (int j = y1; j < y2; j++){
+            arr[j][i] = 1;
+        }
+    }
+}
+void paint2(int x1, int y1, int x2, int y2){
+    for (int i = x1; i < x2; i++){
+        for (int j = y1; j < y2; j++){
+            arr[j][i] = 2;
+        }
+    }
+}
+int main() {
+    // Please write your code here.
+    int x1, y1, x2, y2;
+    int offset = 1000;
+    cin >> x1 >> y1 >> x2 >> y2;
+    x1 += offset;
+    y1 += offset;
+    x2 += offset;
+    y2 += offset;
+    paint1(x1, y1, x2, y2);
+    cin >> x1 >> y1 >> x2 >> y2;
+    x1 += offset;
+    y1 += offset;
+    x2 += offset;
+    y2 += offset;
+    paint1(x1, y1, x2, y2);
+    cin >> x1 >> y1 >> x2 >> y2;
+    x1 += offset;
+    y1 += offset;
+    x2 += offset;
+    y2 += offset;
+    paint2(x1, y1, x2, y2);
+    int area = 0;
+    for (int i = 0; i < 2001; i++){
+        for (int j = 0; j < 2001; j++){
+            if (arr[j][i] == 1) area++;
+        }
+    }
+    cout << area;
+    return 0;
+}
