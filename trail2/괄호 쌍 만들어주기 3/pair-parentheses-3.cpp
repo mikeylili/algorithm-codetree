b04dd4e618cac599212dedb96ba9e1b3b015c968
HEECHANG SON <73211854+mikeylili@users.noreply.github.com>
2026-06-02 17:25:19 +0900

    [Passed] 괄호 쌍 만들어주기 3 (2ms, 0MB)

---
+++ trail2/괄호 쌍 만들어주기 3/pair-parentheses-3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string A;
+
+int main() {
+    cin >> A;
+    int cnt = 0;
+    for (int i = 0; i < A.length(); i++){
+        if (A[i] == '('){
+            for (int j = i+1; j < A.length(); j++){
+            if (A[j] == ')') {
+                cnt++;
+            }
+            }
+        }
+    }
+    cout << cnt;
+    // Please write your code here.
+
+    return 0;
+}
